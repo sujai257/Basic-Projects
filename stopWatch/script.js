98785add62bef25c99151213bc54e7b3a8922b95
sujai257 <sujaisujai257@gmail.com>
2023-08-14 21:20:54 +0530

    .

--- stopWatch/script.js
+++ stopWatch/script.js
@@ -1,51 +1,60 @@
-const clockDisplay = document.querySelector('.clock');
-        let startButton = document.querySelector('.start');
-        const stopButton = document.querySelector('.stop');
-        const resetButton = document.querySelector('.reset');
-        let lapButton = document.querySelector(".lap");
-        let lap =document.querySelector(".lap-times");
-        let count =0;
-        let startTime;
-        let intervalId;
-        let time;
-        function updateClock() {
-            const currentTime = new Date().getTime();
-            const elapsedTime = currentTime - startTime;
-            const minutes = Math.floor(elapsedTime / 60000);
-            const seconds = Math.floor((elapsedTime % 60000) / 1000);
-            const milliseconds = Math.floor((elapsedTime % 1000) / 100);
-             time=`${minutes.toString().padStart(2,0)}:${seconds.toString().padStart(2,0)}.${milliseconds.toString().padStart(2,0)}`;
-            clockDisplay.textContent = time;
-        }
-        lapButton.addEventListener('click', function() {
-            if (startTime) {
-                count++;
-                lap.innerHTML+=`<p>${count} -> ${time}<p>`
-            }
-        }); 
-        startButton.addEventListener('click', function() {
-            if (!startTime) {
-                startButton.disabled= true;
-                stopButton.disabled = false;
-                resetButton.disabled= true;
-                startTime = new Date().getTime();
-                intervalId = setInterval(updateClock, 100);
-            }
-        });
-        
-        stopButton.addEventListener('click', function() {
-            clearInterval(intervalId);
-            startTime = undefined;
-            stopButton.disabled = true;
-            startButton.disabled=false;
-            resetButton.disabled= false;
-        });
-        
-        resetButton.addEventListener('click', function() {
-            clearInterval(intervalId);
-            startTime = undefined;
-            clockDisplay.textContent = '00:00.00';
-            stopButton.disabled= false;
-            lap.innerHTML="Laps"
-            count =0;
-        });
+const clockDisplay = document.querySelector(".clock");
+const startButton = document.querySelector(".start");
+const stopButton = document.querySelector(".stop");
+const resetButton = document.querySelector(".reset");
+const lapButton = document.querySelector(".lap");
+const lap = document.querySelector(".lap-times");
+let count = 0;
+let startTime;
+let intervalId;
+let time;
+
+function updateClock() {
+  const currentTime = new Date().getTime();
+  const elapsedTime = currentTime - startTime;
+  const minutes = Math.floor(elapsedTime / 60000);
+  const seconds = Math.floor((elapsedTime % 60000) / 1000);
+  const milliseconds = Math.floor((elapsedTime % 1000) / 100);
+  time = `${minutes.toString().padStart(2, "0")}:${seconds
+    .toString()
+    .padStart(2, "0")}:${milliseconds.toString().padStart(2, "0")}`;
+  clockDisplay.textContent = time;
+}
+
+lapButton.addEventListener("click", function () {
+  if (startTime) {
+    count++;
+    lap.innerHTML += `${count} -> ${time}<br>`;
+  }
+});
+
+startButton.addEventListener("click", function () {
+  if (!startTime) {
+    startButton.disabled = true;
+    stopButton.disabled = false;
+    resetButton.disabled = true;
+    startTime = new Date().getTime() - (time ? convertToMilliseconds(time) : 0);
+    intervalId = setInterval(updateClock, 100);
+  }
+});
+function convertToMilliseconds(timeString) {
+  const [minutes, seconds, milliseconds] = timeString.split(/[::]/).map(Number);
+  return minutes * 60000 + seconds * 1000 + milliseconds * 100;
+}
+
+stopButton.addEventListener("click", function () {
+  clearInterval(intervalId);
+  startTime = undefined;
+  stopButton.disabled = true;
+  startButton.disabled = false;
+  resetButton.disabled = false;
+});
+
+resetButton.addEventListener("click", function () {
+  clearInterval(intervalId);
+  startTime = undefined;
+  clockDisplay.textContent = "00:00:00";
+  stopButton.disabled = true;
+  lap.innerHTML = "Laps<br>";
+  count = 0;
+});
